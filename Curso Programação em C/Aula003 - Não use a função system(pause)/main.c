0b890062e12dad8177ad84bc7f59fd7cebf55495
Thallonys <thallonys@protonmail.ch>
2022-05-15 12:37:07 -0300

    Uso do getchar(); em vez de system(pause)

---
+++ Curso Programação em C/Aula003 - Não use a função system(pause)/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// system("pause");
+
+int main()
+{
+    printf("\n----------------------------------------\n\n"
+            "1 - Logar  2 - Cadastrar   3 - Imprimir\n"
+            "\n----------------------------------------\n");
+
+    printf("\nValor retornado: %d", printf("bom"));
+
+    printf("\nPressione qualquer tecla para finalizar.");
+    getchar();
+}
